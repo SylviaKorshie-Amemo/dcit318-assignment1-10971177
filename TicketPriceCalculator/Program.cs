0fae5ae832473c6f20fe6ae785c57b5298b80d3c
Sylvia Korshie-Amemo <sekorshie-amemo@st.ug.edu.gh>
2024-06-02 21:56:14 -0700

    Grade generator

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class TicketPriceCalculator
+{
+    int ticketPrice = 10;
+    int discountPrice = 7;
+    int age;
+
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter age:");
+    }
+
+}
